515b5de04aefecc0295e119feb8c3440250bdfcd
seanarch <zhuqingyun@outlook.com>
2023-06-28 11:48:41 -0600

    update

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const EmployeeList = () => {
+  return (
+    <div>
+      <div className="row">
+        <div className="col">Empolyee List</div>
+        <div className="col">Employee Information</div>
+      </div>
+    </div>
+  );
+};
+
+export default EmployeeList;
