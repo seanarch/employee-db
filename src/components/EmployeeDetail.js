d5db5a0f9da92720ad2c12cce8740b0b7483738e
seanarch <zhuqingyun@outlook.com>
2023-06-28 17:02:04 -0600

    Render detail information

--- src/components/EmployeeDetail.js
+++ src/components/EmployeeDetail.js
@@ -6,7 +6,16 @@ const EmployeeDetail = ({ employees, id }) => {
       {employees
         .filter((employee) => employee.id === id)
         .map((emp) => (
-          <div key={emp.id}>{emp.email}</div>
+          <div key={emp.id}>
+            <img src={emp.imageUrl} alt="" />
+            <div>
+              {emp.firstName} {emp.lastName} ({emp.age})
+            </div>
+            <div>{emp.address}</div>
+            <div>{emp.email}</div>
+            <div>Mobile - {emp.contactNumber}</div>
+            <div>DOB - {emp.dob}</div>
+          </div>
         ))}
     </div>
   );
