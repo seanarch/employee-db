2530f8b4903a1ca8870487d0bcc0aeac757d2f5f
seanarch <zhuqingyun@outlook.com>
2023-06-29 14:50:24 -0600

    Add form

--- src/components/EmployeeForm.js
+++ src/components/EmployeeForm.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 const EmployeeForm = () => {
+  const [startDate, setStartDate] = useState(new Date());
   return (
     <div
       className="modal fade"
@@ -24,7 +28,43 @@ const EmployeeForm = () => {
               aria-label="Close"
             ></button>
           </div>
-          <div className="modal-body">...</div>
+          <div className="modal-body">
+            <form>
+              <div className="mb-2">
+                <input type="text" className="col-6" placeholder="First name" />
+                <input type="text" className="col-6" placeholder="Last name" />
+              </div>
+              <div className="mb-2">
+                <input
+                  type="text"
+                  className="col-12"
+                  placeholder="Image URL (Optional)"
+                />
+              </div>
+              <div className="mb-2">
+                <input
+                  type="email"
+                  className="col-12"
+                  id="exampleInputEmail1"
+                  placeholder="Email"
+                />
+              </div>
+              <div className="mb-2">
+                <input type="text" className="col-12" placeholder="Contact" />
+              </div>
+              <div className="mb-2">
+                <input type="text" className="col-12" placeholder="Salary" />
+              </div>
+              <div className="mb-2">
+                <input type="text" className="col-12" placeholder="Address" />
+              </div>
+              <DatePicker
+                className="col-12"
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+              />
+            </form>
+          </div>
           <div className="modal-footer">
             <button
               type="button"
